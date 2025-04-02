88229c88db60b0844c3be03881b1df16d6237bc1
atomeMIF <atome@milaifontanals.org>
2025-04-02 09:36:37 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <time.h>
+#include <stdlib.h>
+
+srand(time(NULL));   // Initialization, should only be called once.
+int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
+
+printf("Random: %d\n",r);
